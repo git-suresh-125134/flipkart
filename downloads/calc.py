799aab6e35eb4275173ae512ff8ad5eb103548fa
sureshreddy <suresh125134@gmail.com>
2024-09-25 18:55:28 +0530

    hi

---
+++ downloads/calc.py
@@ -0,0 +1,20 @@
+def ad(a,b):
+    return a+b
+
+def mul(a,b):
+    return a*b
+
+def dif(a,b):
+    if a>b:
+        return a-b
+    else:
+        return b-a
+
+def div(a,b):
+    if a>b:
+        return a//b
+    else:
+        return b//a
+
+
+
